809254798e95b67546227c3e8e3aabb23a1cf68b
moabs <souzamoab@gmail.com>
2021-09-16 22:21:18 -0300

    Calling hr-worker with RestTemplate

--- hr-payroll/src/main/java/com/msdev/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/msdev/hrpayroll/HrPayrollApplication.java
@@ -2,8 +2,9 @@ package com.msdev.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class HrPayrollApplication {
 
     public static void main(String[] args) {
--- hr-payroll/src/main/java/com/msdev/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/msdev/hrpayroll/services/PaymentService.java
@@ -1,13 +1,30 @@
 package com.msdev.hrpayroll.services;
 
 import com.msdev.hrpayroll.entities.Payment;
+import com.msdev.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String hrWorkerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(Long workerId, Integer days) {
-        return new Payment("Moab", 100.0, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", String.valueOf(workerId));
+
+        Worker worker = restTemplate.getForObject(hrWorkerHost + "/workers/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 
 }
