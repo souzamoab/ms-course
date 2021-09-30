9b438495181dd2c57b5407cbdd3940d0c87f92e0
moabs <souzamoab@gmail.com>
2021-09-30 14:31:10 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/msdev/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/msdev/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package com.msdev.hrpayroll.resources;
 
 import com.msdev.hrpayroll.entities.Payment;
 import com.msdev.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +17,15 @@ public class PaymentResource {
     @Autowired
     private PaymentService paymentService;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
         Payment payment = paymentService.getPayment(workerId, days);
+        return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+        Payment payment = new Payment("Moab", 400.0, days);
         return ResponseEntity.ok(payment);
     }
 
