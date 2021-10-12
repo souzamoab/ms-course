1a5bb784d091eef3d19348465354bc77d7f967e2
moabs <souzamoab@gmail.com>
2021-10-11 21:20:17 -0300

    Login and JWT token generation

--- hr-oauth/src/main/java/com/msdev/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/msdev/hroauth/services/UserService.java
@@ -5,10 +5,13 @@ import com.msdev.hroauth.feignClients.UserFeignClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -28,4 +31,17 @@ public class UserService {
         return user;
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userFeignClient.findByEmail(username).getBody();
+
+        if(user == null) {
+            logger.error("User not found: " + username);
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        logger.info("User found: " + username);
+
+        return user;
+    }
 }
